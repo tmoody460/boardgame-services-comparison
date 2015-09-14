cc06100d3f7730d8f05ce4c95f0fa4a96fc00b74
Tracy Moody <tracyangelamoody@gmail.com>
2015-09-14 14:18:27 -0500

    Extend to scrape all games from spielbound

--- spielbound/spielbound/items.py
+++ spielbound/spielbound/items.py
@@ -7,8 +7,7 @@
 
 import scrapy
 
-
 class SpielboundItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    title = scrapy.Field()
     pass
--- spielbound/spielbound/spiders/game_spider.py
+++ spielbound/spielbound/spiders/game_spider.py
@@ -1,14 +1,31 @@
 import scrapy
 
+from spielbound.items import SpielboundItem
+
 class GameSpider(scrapy.Spider):
     name = "game"
+    count = 0
     allowed_domains = ["spielbound.com"]
     start_urls = [
         "http://spielbound.com/library"
     ]
 
     def parse(self, response):
-        i = 0
+        # Parse all the games on this page
         for sel in response.css("section[id=content]").xpath('.//td'):
-            title = sel.xpath('div/span/div/h2/a/text()').extract()[0]
-            print "Title:", title
+            game = SpielboundItem()
+
+            title = sel.xpath('div/span/div/h2/a/text()').extract()
+
+            # Check for empty td case on the last page of games
+            if len(title) > 0:
+                self.count += 1
+                game['title'] = title[0]
+                yield game
+
+        # Find the next page button and start scraping that page
+        for href in response.css("section[id=content]").xpath('.//ul').css("li[class=pager-next] > a::attr('href')"):
+            url = response.urljoin(href.extract())
+            yield scrapy.Request(url, callback=self.parse)
+
+        print self.count
